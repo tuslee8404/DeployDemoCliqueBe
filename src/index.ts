837e729cdf367c520c1ccfc4b5518a73fc0bee07
AnhTuLe102 <leanhtupr3@gmail.com>
2026-02-26 16:44:31 +0700

    fix bug cors

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 4000
 app.use(
   cors({
     // ✅ phải đặt TRÊN CÙNG
-    origin: 'http://localhost:8080',
+    origin: ['http://localhost:8080', 'https://deploydemocliquefe1.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
